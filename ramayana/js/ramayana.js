ae64db20c2568a9a51ee4646b2a6d4809d64c8d0
sitalatma <sitalatma@gmail.com>
2022-04-04 14:39:38 +0700

    Initial commit

--- ramayana/js/ramayana.js
+++ ramayana/js/ramayana.js
@@ -11,7 +11,7 @@
     if (window.getComputedStyle(x, null).display === "none") {
       x.style.display = "block";
       //btn.style.transform = 'rotate(-90deg)';
-      btn.innerHTML = "<span>▲</span>";
+      btn.innerHTML = '<span class="stretch">▲</span>';
       var sp = document.getElementById(id).firstChild;
       //var st = getComputedStyle(sp, null);
       var st = sp.style;
@@ -27,7 +27,7 @@
     } else {
       x.style.display = "none";
       //btn.style.transform = 'rotate(0deg)';
-      btn.innerHTML = "<span>▼</span>";
+      btn.innerHTML = '<span class="stretch">▼</span>';
       //sp.firstChild = "▼";
       //console.log('t : ' + t);
     }
